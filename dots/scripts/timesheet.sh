832fe19aef505ebb7b4ec10aed0696ade5192988
Kristian Nymann Jakobsen <kristian@nymann.dev>
2020-03-25 21:20:09 +0100

    muh leaks kappa

--- dots/scripts/timesheet.sh
+++ dots/scripts/timesheet.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
 
-gid="Z003WAEF"
-
 for year in $(seq 2020 2021); do
 	mkdir -p ~/work/timesheets/${year}
 	for week in $(seq -w 1 53); do
-		cp ~/work/timesheets/backup.xlsx ~/work/timesheets/${year}/Z003WAEF_W${week}_${year}.xlsx
+		cp ~/work/timesheets/backup.xlsx ~/work/timesheets/${year}/$1_W${week}_${year}.xlsx
 	done
 done
